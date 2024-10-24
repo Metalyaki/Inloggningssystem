88620cc6855e4118d29e298477d9febcd5e0998a
Alexander Andersson <alexander.andersson94@outlook.com>
2024-10-24 14:09:35 +0200

    adminadmin

--- Inloggningssytem2/Program.cs
+++ Inloggningssytem2/Program.cs
@@ -2,28 +2,32 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
+        public string UserName { get; set; }
+        public string PassWord { get; set; }
 
+
+        public Program(string adminUsernNme, string adminPassWord)
+        {
+            UserName = adminUsernNme;
+            PassWord = adminPassWord;
         }
         static void BytTextFärg()
         {
             Random random = new Random();
             ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
-            ConsoleColor randomColor = colors[Random.Next(colors.Leangth)];
-            Console.ForgroundColor = randomColor;
+            ConsoleColor randomColor = colors[random.Next(colors.Length)];
+            Console.ForegroundColor = randomColor;
             Console.WriteLine($"Textfärgen har ändrats till {randomColor}");
             Console.ResetColor();
             Console.WriteLine("Tryck på en valfri knapp för att fortsätta...");
             Console.ReadKey();
         }
 
-
         static void BytKonsolTitel()
         {
             Console.WriteLine("Ange ny titel för konsolfönstret: ");
             string nyTitel = Console.ReadLine();
-            Console.Titel = nyTitel;
+            Console.Title = nyTitel;
             Console.WriteLine($"Konsolfönstrets titel har ändrats till \"{nyTitel}\"");
             Console.WriteLine("Tryck på valfri knapp för att fortsätta...");
             Console.ReadKey();
