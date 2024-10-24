e046997930a161f5788a2ae900f76637e3578e03
Pontus Rodmalm <pontusr@gmail.com>
2024-10-24 14:14:20 +0200

    gjort klasser för användarnamn och lösen ord och metoder för att returna.

--- Inloggningssytem2/user.cs
+++ Inloggningssytem2/user.cs
@@ -2,7 +2,27 @@
 
 public class user
 {
+	private string userName { get; set; }
+	private string Password { get; set; }
+
 	public user()
 	{
+		Console.WriteLine("Skriv in ett namn: ")
+
+		string name = Console.ReadLine();
+		Console.WriteLine("Skriv in ett lösenord: ");
+		string lösenord = Console.ReadLine();
+
+		userName = name;
+		Password = lösenord;
 	}
+	public string Namn()
+	{
+		return userName;
+	}
+	public string Lösenord()
+	{
+		return Password;
+	}
+}
 }
