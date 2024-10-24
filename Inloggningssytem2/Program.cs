0df884ac303680deb174bd617d8ddceae67cf229
Alexander Andersson <alexander.andersson94@outlook.com>
2024-10-24 13:36:45 +0200

    admin

--- Inloggningssytem2/Program.cs
+++ Inloggningssytem2/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+        }
+        static void BytTextFärg()
+        {
+            Random random = new Random();
+            ConsoleColor[] colors = (ConsoleColor[])ConsoleColor.GetValues(typeof(ConsoleColor));
+            ConsoleColor randomColor = colors[Random.Next(colors.Leangth)];
+            Console.ForgroundColor = randomColor;
+            Console.WriteLine($"Textfärgen har ändrats till {randomColor}");
+            Console.ResetColor();
+            Console.WriteLine("Tryck på en valfri knapp för att fortsätta...");
+            Console.ReadKey();
+        }
+
+
+        static void BytKonsolTitel()
+        {
+            Console.WriteLine("Ange ny titel för konsolfönstret: ");
+            string nyTitel = Console.ReadLine();
+            Console.Titel = nyTitel;
+            Console.WriteLine($"Konsolfönstrets titel har ändrats till \"{nyTitel}\"");
+            Console.WriteLine("Tryck på valfri knapp för att fortsätta...");
+            Console.ReadKey();
         }
     }
 }
