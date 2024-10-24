5bbf2947694678fbb15606a64d0042de6358f167
Metalyaki <alfons.newberg@live.se>
2024-10-24 14:24:21 +0200

    asdddd

--- Inloggningssytem2/User.cs
+++ Inloggningssytem2/User.cs
@@ -6,7 +6,31 @@ using System.Threading.Tasks;
 
 namespace Inloggningssytem2
 {
-    internal class User
+    public class user
     {
+        private string userName { get; set; }
+        private string Password { get; set; }
+
+        public user()
+        {
+            Console.WriteLine("Skriv in ett namn: ");
+    
+
+        string name = Console.ReadLine();
+            Console.WriteLine("Skriv in ett lösenord: ");
+            string lösenord = Console.ReadLine();
+
+            userName = name;
+            Password = lösenord;
+        }
+        public string Namn()
+        {
+            return userName;
+        }
+        public string Lösenord()
+        {
+            return Password;
+        }
     }
 }
+
