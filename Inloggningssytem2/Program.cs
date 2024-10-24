57fc0e51d5def99cb14850fbc91e107040113e72
Anders Nguyen <anders.nguyen@learnet.se>
2024-10-24 13:43:08 +0200

    exempel på start menu

--- Inloggningssytem2/Program.cs
+++ Inloggningssytem2/Program.cs
@@ -4,7 +4,76 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+
+            Console.WriteLine("Username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Password: ");
+            string password = Console.ReadLine();
+
+            if(username == admin.username && password == admin.password)
+            {
+                while (true)
+                {
+                    Console.WriteLine("1. Lägg till användare");
+
+                    Console.WriteLine("2. Inställningar");
+
+                    Console.WriteLine("3. Avsluta");
+
+                    userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            break;
+                        case "2":
+                            Console.WriteLine("1. Byt till slumpmässig textfärg");
+
+                            Console.WriteLine("2. Ändra konsolfönstrets titel");
+
+                            break;
+                        case "3":
+                            System.Environment.Exit(0);
+                            break;
+
+                        default:
+                            Console.WriteLine("Fel menyval");
+                            break;
+                    }
+
+                }
+
+            }
+
+            if(username == user.username && password == user.password)
+            {
+                while (true)
+                {
+                    Console.WriteLine("1. Visa lösenord");
+
+                    Console.WriteLine("2. Avsluta");
+
+                    userInput = Console.ReadLine();
+
+                    switch (userInput)
+                    {
+                        case "1":
+                            break;
+                        case "2":
+                            break;
+                        default:
+                            Console.WriteLine("Fel menyval");
+                            break;
+                    }
+                }
+
+            }
+
+            
+
+           
         }
     }
 }
