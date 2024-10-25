df52868e96b2f8c5502f5d987f0c9364e1328dcd
Metalyaki <alfons.newberg@live.se>
2024-10-25 14:35:27 +0200

    Timerfunktion implementerad Logga ut funktion implementerad

--- Inloggningssytem2/Program.cs
+++ Inloggningssytem2/Program.cs
@@ -4,79 +4,147 @@
     {
         static void Main(string[] args)
         {
-            string userInput;
+            List<user> users = new List<user>();
+            Admin admin = new Admin();
 
-            Console.WriteLine("Username: ");
-            string username = Console.ReadLine();
-
-            Console.WriteLine("Password: ");
-            string password = Console.ReadLine();
-
-            if (username == admin.username && password == admin.password)
+            while (true)
             {
-                while (true)
+                bool menuAdmin = true;
+                bool menuUser = true;
+                string userInput;
+
+                Console.Clear();
+
+                Console.WriteLine("Inloggningssystem");
+
+                Console.WriteLine("Användarnamn: ");
+                string username = Console.ReadLine();
+
+                Console.WriteLine("Lösenord: ");
+                string password = Console.ReadLine();
+
+                if (username == admin.UserName && password == admin.PassWord)
                 {
-                    Console.WriteLine("1. Lägg till användare");
+                    DateTime adminDateTime = DateTime.Now;
+                    TimeSpan duration = TimeSpan.FromMinutes(10);
 
-                    Console.WriteLine("2. Inställningar");
-
-                    Console.WriteLine("3. Avsluta");
-
-                    userInput = Console.ReadLine();
-
-                    switch (userInput)
+                    while (menuAdmin)
                     {
-                        case "1":
+                        TimeSpan elapsed = DateTime.Now - adminDateTime;
+                        if (elapsed > duration)
+                        {
+                            Console.Clear();
+                            Console.WriteLine("Max inloggingstid uppnådd. Du loggas ut.");
+                            Thread.Sleep(2000);
+                            menuAdmin = false;
                             break;
-                        case "2":
-                            Console.WriteLine("1. Byt till slumpmässig textfärg");
+                        }
 
-                            Console.WriteLine("2. Ändra konsolfönstrets titel");
+                        Console.Clear();
 
-                            break;
-                        case "3":
-                            System.Environment.Exit(0);
-                            break;
+                        Console.WriteLine("1. Lägg till användare");
 
-                        default:
-                            Console.WriteLine("Fel menyval");
-                            break;
+                        Console.WriteLine("2. Inställningar");
+
+                        Console.WriteLine("3. Logga ut");
+
+                        Console.WriteLine("4. Avsluta");
+
+                        userInput = Console.ReadLine();
+
+                        switch (userInput)
+                        {
+                            case "1":
+                                users.Add(new user());
+
+                                break;
+                            case "2":
+                                Console.Clear();
+
+                                Console.WriteLine("1. Byt till slumpmässig textfärg");
+
+                                Console.WriteLine("2. Ändra konsolfönstrets titel");
+
+                                userInput = Console.ReadLine();
+
+                                switch (userInput)
+                                {
+                                    case "1":
+                                        admin.BytTextFärg();
+                                        break;
+                                    case "2":
+                                        admin.BytKonsolTitel();
+                                        break;
+                                    default:
+                                        Console.WriteLine("Fel menyval");
+                                        break;
+                                }
+                                break;
+                            case "3":
+                                menuAdmin = false;
+                                break;
+
+                            case "4":
+                                System.Environment.Exit(0);
+                                break;
+
+                            default:
+                                Console.WriteLine("Fel menyval");
+                                break;
+                        }
+
                     }
 
                 }
 
-            }
-
-            if (username == user.username && password == user.password)
-            {
-                while (true)
+                foreach (var user in users)
                 {
-                    Console.WriteLine("1. Visa lösenord");
-
-                    Console.WriteLine("2. Avsluta");
-
-                    userInput = Console.ReadLine();
-
-                    switch (userInput)
+                    if (user.Namn() == username && user.Lösenord() == password)
                     {
-                        case "1":
-                            break;
-                        case "2":
-                            break;
-                        default:
-                            Console.WriteLine("Fel menyval");
-                            break;
+                        DateTime userDateTime = DateTime.Now;
+                        TimeSpan duration = TimeSpan.FromMinutes(2);
+
+                        while (menuUser)
+                        {
+                            TimeSpan elapsed = DateTime.Now - userDateTime;
+                            if (elapsed > duration)
+                            {
+                                Console.Clear();
+                                Console.WriteLine("Max inloggingstid uppnådd. Du loggas ut.");
+                                Thread.Sleep(2000);
+                                menuUser = false;
+                                break;
+                            }
+
+                            Console.Clear();
+
+                            Console.WriteLine("1. Visa lösenord");
+
+                            Console.WriteLine("2. Logga ut");
+
+                            Console.WriteLine("3. Avsluta");
+
+                            userInput = Console.ReadLine();
+
+                            switch (userInput)
+                            {
+                                case "1":
+                                    Console.WriteLine(user.Lösenord());
+                                    break;
+                                case "2":
+                                    menuUser = false;
+                                    break;
+                                case "3":
+                                    System.Environment.Exit(0);
+                                    break;
+                                default:
+                                    Console.WriteLine("Fel menyval");
+                                    break;
+                            }
+                        }
                     }
                 }
-
             }
-
         }
-            
-
-            
-
-           
-        
     }
 }
